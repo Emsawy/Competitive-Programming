8795deb9d8efcaac2e4a6b38ea587f0f59bc480c
Emsawy <M.EmadAhmed101@gmail.com>
2017-02-09 18:26:39 +0200

    Update SRM352-D2-1000.cpp

--- Topcoder/SRM352-D2-1000.cpp
+++ Topcoder/SRM352-D2-1000.cpp
@@ -23,17 +23,17 @@ using namespace std;
 
 class RaceManagement {
 public:
-	vector<int>p, a;
+	vector<double>p, a;
 	long long sum = 0;
 	double res = 0;
 	double go(int id, int win, int cnt, double po , double f){
 		if (id == a.size()){
-			if (cnt == 1) return (((sum - a[win]) - a[win] * f) * (po / 100.0));
-			return (sum * (po / 100.0));
+			if (cnt == 1) return (sum - a[win] - a[win] * f) * po;
+			return sum * po;
 		}
 		double ret = 0;
-		ret += go(id + 1, id, cnt + 1, (po * p[id]) / 100 , f);
-		ret += go(id + 1, win, cnt, (po * (100 - p[id])) / 100 , f);
+		ret += go(id + 1, id, cnt + 1, po * p[id], f);
+		ret += go(id + 1, win, cnt, po * (1 - p[id]), f);
 		return ret;
 	}
 	bool comp(double a, double b){
@@ -42,18 +42,17 @@ public:
 		return 0;
 	}
 	double getPayoutFactor(vector <int> probability, vector <int> amounts, int minimumMoney) {
-		p = probability, a = amounts;
-		for (int i = 0; i < a.size(); i++)
-			sum += a[i];
+		for (int i = 0; i < probability.size(); i++)
+			p.push_back(probability[i] / 100.0), a.push_back(amounts[i]), sum += a[i];
 
 		int lp = 100000;
 		double s = -10000, e = 10000, rs = 0;
 		while (lp --){
 			double mid = (s + e) / 2.0;
-			if (comp(go(0, -1, 0, 100, mid), minimumMoney)) e = mid - 1, rs = mid;
+			if (comp(go(0, -1, 0, 1, mid), minimumMoney)) e = mid - 1, rs = mid;
 			else s = mid + 1 ;
 		}
-		if (go(0, -1, 0, 100, 1e6) >= minimumMoney && go(0, -1, 0, 100, -1e6) >= minimumMoney) return -2;
+		if (go(0, -1, 0, 1, 1e6) >= minimumMoney && go(0, -1, 0, 1, -1e6) >= minimumMoney) return -2;
 		if (rs < 0) return -1.0;
 		return rs;
 	}

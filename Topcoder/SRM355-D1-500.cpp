6a4fd8a25e62111c142608a8005e2505edd676da
Emsawy <M.EmadAhmed101@gmail.com>
2017-03-02 13:09:56 +0200

    Create SRM355-D1-500.cpp

---
+++ Topcoder/SRM355-D1-500.cpp
@@ -0,0 +1,71 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+/*
+http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=srm355
+*/
+
+class Tetrahedron {
+public:
+	double dis(double x, double y, double a, double b){
+		return sqrt((x - a) * (x - a) + (y - b) * (y - b));
+	}
+	bool comp(double a, double b){
+		if (abs(a - b) <= 1e-9) return 1;
+		if (a < b) return 1;
+		return 0;
+	}
+	string exists(vector <string> d) {
+		vector<pair<int, int> > po;
+		for (int i = 0; i < 10; i++){
+			for (int j = 0; j < 10; j++){
+				po.push_back({ i, j });
+			}
+		}
+		int t;
+		vector<vector<int> > v(4);
+		for (int i = 0; i < d.size(); i++){
+			stringstream ss;
+			ss << d[i];
+			while (ss >> t) v[i].push_back(t);
+		}
+		int AB = v[0][1];
+		int AC = v[0][2];
+		int AD = v[0][3];
+		int BC = v[1][2];
+		int BD = v[1][3];
+		int CD = v[2][3];
+		
+		double A1 = ((AC * AC) + (AB * AB) - (BC * BC)) / (2.0 * AC * AB);
+		double X1 = AC * A1;
+		double Y1 = sqrt(AC * AC - X1 * X1);
+
+		double A2 = ((AD * AD) + (AB * AB) - (BD * BD)) / (2.0 * AD * AB);
+		double X2 = AD * A2;
+		double Y2 = sqrt(AD * AD - X2 * X2);
+		
+		double mn = dis(X1, Y1, X2, Y2);
+		double mx = dis(X1, Y1, X2, -Y2);
+
+		if (comp(CD, mx) && comp(mn, CD)) return "YES";
+		return "NO";
+	}
+};

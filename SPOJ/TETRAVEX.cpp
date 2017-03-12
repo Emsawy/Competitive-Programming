2c83f1c96ef26738ef7a3104daa2746838be7816
Emsawy <M.EmadAhmed101@gmail.com>
2017-03-12 02:25:24 +0200

    Update TETRAVEX.cpp

--- SPOJ/TETRAVEX.cpp
+++ SPOJ/TETRAVEX.cpp
@@ -30,7 +30,6 @@ void Emsawy(){
 #define rotate0(p,ang)	((p)*exp(Point(0,ang)))
 #define rotateA(p,ang,A)(rotate0(vec(A,p),ang)+A)
 #define reflect0(v,m)	(conj((v)/(m))*(m))
-#define DEBUG	system("Puase")
 #define pii		pair<int,int>
 #define V		vector
 #define MP		make_pair
@@ -38,7 +37,6 @@ void Emsawy(){
 #define Y		imag()
 typedef complex<double> Point;
 typedef long long ll;
-typedef pair<pair<int, int>, pair<int, int> > tile;
 
 const double PI = acos(-1.0);
 const double EPS = acos(1e-10);
@@ -46,52 +44,63 @@ const ll mod = ll(1e9 + 7), oo = ll(1e9);
 
 V<V<pii> > adj;
 int n, m, k;
+struct tile{
+	int u, l, d, r;
+	bool operator<(tile & x) const{
+		if (u < x.u) return 1;
+		else if (u == x.u && l < x.l) return 1;
+		else if (u == x.u && l == x.l && d < x.d) return 1;
+		else if (u == x.u && l == x.l && d == x.d && r < x.r) return 1;
+		return 0;
+	}
+};
 
+tile g[3][3];
+bool valid(){
+	if (g[0][0].l != g[0][1].r) return 0;
+	if (g[0][1].l != g[0][2].r) return 0;
+
+	if (g[1][0].l != g[1][1].r) return 0;
+	if (g[1][1].l != g[1][2].r) return 0;
+
+	if (g[2][0].l != g[2][1].r) return 0;
+	if (g[2][1].l != g[2][2].r) return 0;
+
+	if (g[1][0].u != g[0][0].d) return 0;
+	if (g[1][1].u != g[0][1].d) return 0;
+	if (g[1][2].u != g[0][2].d) return 0;
+
+	if (g[1][0].d != g[2][0].u) return 0;
+	if (g[1][1].d != g[2][1].u) return 0;
+	if (g[1][2].d != g[2][2].u) return 0;
+
+	return 1;
+}
 int main()
 {
 	Emsawy();
-	int c = 1;
-	int t;
+	int t , c = 1;
 	cin >> t;
 	while (t--){
-		if (c > 1)puts("");
+		if (c > 1) puts("");
 		V<tile>v(9);
 		for (int i = 0; i < 9; i++){
-			cin >> v[i].first.first >> v[i].first.second >> v[i].second.first >> v[i].second.second;
+			cin >> v[i].u >> v[i].l >> v[i].d >> v[i].r;
 		}
 		sort(all(v));
-		tile g[3][3];
 		bool ok = 0;
 		do{
 			int i = 0;
 			while (i < 3) g[0][i] = v[i], i++;
-			while (i < 6) g[1][i % 3] = v[i], i++;
-			while (i < 9) g[2][i % 3] = v[i], i++;
-			int ret = 1;
-			if (g[0][0].first.second != g[0][1].second.second) ret = 0;
-			if (g[0][1].first.second != g[0][2].second.second) ret = 0;
-
-			if (g[1][0].first.second != g[1][1].second.second) ret = 0;
-			if (g[1][1].first.second != g[1][2].second.second) ret = 0;
-
-			if (g[2][0].first.second != g[2][1].second.second) ret = 0;
-			if (g[2][1].first.second != g[2][2].second.second) ret = 0;
-
-			if (g[1][0].first.first != g[0][0].second.first) ret = 0;
-			if (g[1][1].first.first != g[0][1].second.first) ret = 0;
-			if (g[1][2].first.first != g[0][2].second.first) ret = 0;
-
-			if (g[1][0].second.first != g[2][0].first.first) ret = 0;
-			if (g[1][1].second.first != g[2][1].first.first) ret = 0;
-			if (g[1][2].second.first != g[2][2].first.first) ret = 0;
-
-			if (ret == 1){
+			while (i < 6) g[1][i%3] = v[i], i++;
+			while (i < 9) g[2][i%3] = v[i], i++;
+			if (valid()) {
 				ok = 1;
 				break;
 			}
 		} while (next_permutation(all(v)));
-		if (ok)printf("Case %d: YES\n", c++);
-		else printf("Case %d: NO\n", c++);
+		if (ok) printf("Case %d: YES\n",c++);
+		else printf("Case %d: NO\n",c++);
 	}
 	return 0;
 }

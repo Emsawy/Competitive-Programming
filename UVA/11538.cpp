97c52473613a11efcfe394eccd671576cf80ac13
Emsawy <M.EmadAhmed101@gmail.com>
2017-03-15 17:32:51 +0200

    Create 11538.cpp

---
+++ UVA/11538.cpp
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+void Emsawy(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+#define sz(v)			((int)((v).size()))
+#define all(v)			((v).begin()), ((v).end())
+#define allr(v)			((v).rbegin()), ((v).rend())
+#define clr(v,d)		memset(v, d, sizeof(v))
+#define sf(x)			scanf("%d", &x)
+#define sf2(x,y)		scanf("%d %d", &x, &y)
+#define sf3(x,y,z)		scanf("%d %d %d", &x, &y, &z)
+#define sfll(x)			scanf("%I64d", &x)
+#define sfll2(x,y)		scanf("%I64d %I64d", &x, &y)
+#define sfll3(x,y,z)		scanf("%I64d %I64d %I64d", &x, &y, &z)
+#define angle(a)		(atan2((a).imag(), (a).real()))
+#define vec(a,b)		((b)-(a))
+#define length(a)		(hypot((a).imag(), (a).real()))
+#define dp(a,b)			((conj(a)*(b)).real()) // if zero prep
+#define cp(a,b)			((conj(a)*(b)).imag()) // if zero parl
+#define same(p1,p2)		(dp(vec(p1,p2),vec(p1,p2)) <= EPS)
+#define rotate0(p,ang)		((p)*exp(Point(0,ang)))
+#define rotateA(p,ang,about)	(rotate0(vec(about,p),ang)+about)
+#define reflect0(v,m)		(conj((v)/(m))*(m))
+#define normalize(a)		(a)/length(a)
+#define lengthSqr(p)		dp(p,p)
+#define pii			pair<int,int>
+#define V			vector
+#define MP			make_pair
+#define X			real()
+#define Y			imag()
+
+typedef complex<double> 	point;
+typedef long long ll;
+
+const double PI = acos(-1.0);
+const double EPS = 1e-10;
+const ll mod = ll(1e9 + 7), oo = ll(1e9), si = 1000001;
+
+V<V<pii> > adj;
+int n, m, k, t, c;
+ll pre[si];
+int main()
+{
+	Emsawy();
+	for (int i = 1; i < si; i++){
+		pre[i] = (i * 1LL * (i - 1));
+		pre[i] += pre[i - 1];
+	}
+	while (cin >> n >> m){
+		if (n == 0 && m == 0) break;
+		if (n > m) swap(n, m);
+		ll ans = 0 , dif = m - n + 1;
+		ans += (pre[n] - pre[n - 1]) * 1LL * (dif * 2 + m);
+		ans += (pre[m] - pre[m - 1]) * 1LL * n;
+		ans += pre[n - 1] * 1LL * 4;
+		printf("%lld\n", ans);
+	}
+	
+	return 0;
+}

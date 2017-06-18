b5a18c63dfce4562b75e31805da33bb6ffa81e67
Emsawy <M.EmadAhmed101@gmail.com>
2017-06-18 08:33:49 +0200

    Create SPOJ ABA12E.cpp

---
+++ SPOJ/SPOJ ABA12E.cpp
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+void Emsawy(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+#define sz(v)			((int)((v).size()))
+#define all(v)			((v).begin()), ((v).end())
+#define allr(v)			((v).rbegin()), ((v).rend())
+#define clr(v,d)		memset(v, d, sizeof(v))
+#define sf(x)			scanf("%d", &x)
+#define sf2(x,y)		scanf("%d %d", &x, &y)
+#define sf3(x,y,z)		scanf("%d %d %d", &x, &y, &z)
+#define sfll(x)			scanf("%I64d", &x)
+#define sfll2(x,y)		scanf("%I64d %I64d", &x, &y)
+#define sfll3(x,y,z)		scanf("%I64d %I64d %I64d", &x, &y, &z)
+#define angle(a)		(atan2((a).imag(), (a).real()))
+#define vec(a,b)		((b)-(a))
+#define length(a)		(hypot((a).imag(), (a).real()))
+#define dp(a,b)			((conj(a)*(b)).real()) // if zero prep
+#define cp(a,b)			((conj(a)*(b)).imag()) // if zero parl
+#define same(p1,p2)		(dp(vec(p1,p2),vec(p1,p2)) <= EPS)
+#define rotate0(p,ang)		((p)*exp(Point(0,ang)))
+#define rotateA(p,ang,about)	(rotate0(vec(about,p),ang)+about)
+#define reflect0(v,m)		(conj((v)/(m))*(m))
+#define normalize(a)		(a)/length(a)
+#define lengthSqr(p)		dp(p,p)
+#define pii			pair<int,int>
+#define V			vector
+#define MP			make_pair
+#define X			real()
+#define Y			imag()
+
+typedef complex<double> 	point;
+typedef long long ll;
+
+const double PI = acos(-1.0);
+const double EPS = 1e-10;
+const ll mod = ll(1e9 + 7), oo = ll(1e9), si = 1000000;
+
+V<V<pii> > adj;
+ll n, m, k, t, c , sum[50005];
+ll valid(ll mid, ll k){
+	ll ret = 0;
+	for (int i = 0; i < n; i++){
+		ll go = upper_bound(sum, sum + n , mid + (i - 1 >= 0 ? sum[i - 1] : 0)) - sum - i;
+		ret += go;
+	}
+	return ret;
+}
+int main()
+{
+	Emsawy();
+	while (cin >> n >> k){
+		V<ll>v(n);
+		for (int i = 0; i < n; i++){
+			cin >> v[i];
+			sum[i] = v[i];
+			if (i) sum[i] += sum[i - 1];
+		}
+		ll s = 0, e = 10, res = 0;
+		while (s <= e){
+			ll mid = (s + e) / 2;
+			ll ret = valid(mid, k);
+			if (ret <= k) s = mid + 1, res = mid;
+			else e = mid - 1;
+		}
+		cout << res << endl;
+	}
+	return 0;
+}

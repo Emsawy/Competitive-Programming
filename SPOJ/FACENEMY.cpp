5d1459b83832a4535ef1e80742ec01798b5e66b6
Emsawy <M.EmadAhmed101@gmail.com>
2017-07-08 01:02:25 +0200

    Create FACENEMY.cpp

---
+++ SPOJ/FACENEMY.cpp
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+void Emsawy(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+#define sz(v)			((int)((v).size()))
+#define all(v)			((v).begin()), ((v).end())
+#define allr(v)			((v).rbegin()), ((v).rend())
+#define clr(v,d)		memset(v, d, sizeof(v))
+#define sf(x)			scanf("%d", &x)
+#define sf2(x,y)		scanf("%d %d", &x, &y)
+#define sf3(x,y,z)		scanf("%d %d %d", &x, &y, &z)
+#define sfll(x)			scanf("%I64d", &x)
+#define sfll2(x,y)		scanf("%I64d %I64d", &x, &y)
+#define sfll3(x,y,z)		scanf("%I64d %I64d %I64d", &x, &y, &z)
+#define angle(a)		(atan2((a).imag(), (a).real()))
+#define vec(a,b)		((b)-(a))
+#define length(a)		(hypot((a).imag(), (a).real()))
+#define dp(a,b)			((conj(a)*(b)).real()) // if zero prep
+#define cp(a,b)			((conj(a)*(b)).imag()) // if zero parl
+#define same(p1,p2)		(dp(vec(p1,p2),vec(p1,p2)) <= EPS)
+#define rotate0(p,ang)		((p)*exp(Point(0,ang)))
+#define rotateA(p,ang,about)	(rotate0(vec(about,p),ang)+about)
+#define reflect0(v,m)		(conj((v)/(m))*(m))
+#define normalize(a)		(a)/length(a)
+#define lengthSqr(p)		dp(p,p)
+#define pii			pair<int,int>
+#define V			vector
+#define MP			make_pair
+#define X			real()
+#define Y			imag()
+
+
+typedef complex<double> 	point;
+typedef long long ll;
+
+const double PI = acos(-1.0);
+const double EPS = 1e-10;
+const ll mod = ll(1e9 + 7), oo = ll(1e9);
+
+V<V<pii> > adj;
+int n, m, k, t, c;
+
+double dis(point a, point b){
+	return sqrt((a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y));
+}
+
+int main()
+{
+	Emsawy();
+	point a, b, c, d;
+	int t , x , y;
+	cin >> t;
+	while (t--){
+		cin >> x >> y; a = point(x, y);
+		cin >> x >> y; b = point(x, y);
+		cin >> x >> y; c = point(x, y);
+		cin >> x >> y; d = point(x, y);
+
+		double l = dis(a, b), r = dis(a, c);
+		double A = acos(dp(b - a, c - a) / (l * r)) * (180 / PI);
+
+		l = dis(c, d), r = dis(c, a);
+		double B = acos(dp(d - c, a - c) / (l * r)) * (180 / PI);
+
+		A = min(A, 360 - A);
+		B = min(B, 360 - B);
+
+		if (fabs(A - B) <= EPS) cout << 0 << endl;
+		else if (A < B) cout << "Hero" << endl;
+		else cout << "Heroine" << endl;
+	}
+	return 0;
+}

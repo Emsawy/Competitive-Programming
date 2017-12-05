31390d498989b5d0d2d12070a84d87bca9a92f08
Emsawy <M.EmadAhmed101@gmail.com>
2017-12-05 13:18:22 +0200

    Create CF448-D2-D.cpp

---
+++ Codeforces/CF448-D2-D.cpp
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+void Emsawy(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios::sync_with_stdio(false);   // don't use scanf & printf
+	cin.tie(0);
+	cout.tie(0);
+}
+
+#define p00          point(0,0)
+#define O            const point & 
+#define CD           const double &  
+#define EQ(a, b)     (fabs((a) - (b)) <= eps) /* equal to */
+#define LT(a, b)     ((a) < (b)-eps) /* less than */
+#define sz(v)	     ((int)((v).size()))
+#define all(v)	     ((v).begin()), ((v).end())
+#define allr(v)	     ((v).rbegin()), ((v).rend())
+#define clr(v,d)     memset(v, d, sizeof(v))
+#define length(a)    (hypot((a).imag(), (a).real()))
+#define dp(a,b)	     ((conj(a)*(b)).real()) // if zero prep
+#define cp(a,b)	     ((conj(a)*(b)).imag()) // if zero parl
+#define pii	         pair<int,int>
+#define V	         vector
+#define MP	         make_pair
+#define bug(n)           cout<< #n <<" = "<< n << endl;
+
+typedef long long ll;
+
+const double PI = acos(-1.0);
+const double eps = 1e-10;
+const int mod = 1e9 + 7, oo = 1e9;
+
+int dx[]{1, -1, 0, 0, 1, -1, 1, -1};
+int dy[]{0, 0, 1, -1, 1, -1, -1, 1};
+
+ll n, m,k, t, c;
+V<V<int> > adj, tree;
+const int N = 1500 + 5;
+
+
+int main()
+{
+	Emsawy();
+	while (cin >> n >> m >> k){
+		ll s = 0, e = 1e12;
+		while (s < e){
+			ll mid = s + (e - s + 1) / 2;
+			ll res = 0;
+			for (int i = 1; i <= n; i++){
+				res += min(m, mid / i);
+			}
+			if (res >= k) e = mid - 1;
+			else s = mid;
+		}
+		cout << s+1 << endl;
+	}
+	return 0;
+}

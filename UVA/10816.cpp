7ce0e0b3931768e4f737a7bd7fa2c53ab1e1a1c6
Emsawy <M.EmadAhmed101@gmail.com>
2017-12-07 04:22:13 +0200

    Update 10816.cpp

--- UVA/10816.cpp
+++ UVA/10816.cpp
@@ -33,10 +33,6 @@ void Emsawy(){
 
 typedef long long ll;
 
-
-float max(float a, float b){ return (a>b) ? a : b; }
-float min(float a, float b){ return (a<b) ? a : b; }
-
 const double PI = acos(-1.0);
 const double eps = 1e-10;
 const ll mod = 1e9 + 7, oo = 1e9;
@@ -47,62 +43,66 @@ int dy[]{0, 0, 1, -1, 1, -1, -1, 1};
 V<V<pii> > adj;
 const int N = 100 + 2;
 int n, m, k, t, c, e, src, sink, u[N*N], v[N*N];
-float a[N*N], b[N*N];
-float T[N][N], D[N][N];
-int par[N][N];
+double a[N*N], b[N*N];
+int T[N][N], D[N][N];
+int path[N][N];
+
+void Path(int sr,int sin, int p){
+	if (!path[sr][sin]){
+		if (!p) cout << sr << " ";
+		cout << sin;
+		if (sin != sink)
+			cout << " ";
+		return;
+	}
+	Path(sr, path[sr][sin], p);
+	Path(path[sr][sin], sin, 1);
+}
 int main()
 {
 	Emsawy();
-	while (scanf("%d%d",&n ,&e) == 2){
+	while (cin >> n >> e){
 		fill(T[0], T[0] + N*N, oo);
 		fill(D[0], D[0] + N*N, oo);
-		scanf("%d%d", &src, &sink);
+		clr(path, 0);
+		cin >> src >> sink;
 		for (int i = 0; i < e; i++){
-			scanf("%d %d %f %f", u + i, v + i, a + i, b + i);
-			T[u[i]][v[i]] = min(T[u[i]][v[i]], a[i]);
-			T[v[i]][u[i]] = min(T[v[i]][u[i]], a[i]);
+			cin >> u[i] >> v[i] >> a[i] >> b[i];
+			a[i] *= 10;
+			b[i] *= 10;
+			T[u[i]][v[i]] = min(T[u[i]][v[i]], int(a[i]));
+			T[v[i]][u[i]] = min(T[v[i]][u[i]], int(a[i]));
 		}
-		for (int k = 1; k <= n; k++){
-			for (int i = 1; i <= n; i++){
-				for (int j = 1; j <= n; j++){
+		for (int k = 0; k <= n; k++){
+			for (int i = 0; i <= n; i++){
+				for (int j = 0; j <= n; j++){
 					T[i][j] = min(T[i][j], max(T[i][k], T[k][j]));
 				}
 			}
 		}
 		for (int i = 0; i < e; i++){
-			if (a[i] < T[src][sink] + 1e-6){
-				D[v[i]][u[i]] = min(D[v[i]][u[i]], b[i]);
-				D[u[i]][v[i]] = min(D[u[i]][v[i]], b[i]);
-				par[v[i]][u[i]] = v[i];
-				par[u[i]][v[i]] = u[i];
-			}
+			if (a[i] > T[src][sink]) continue;
+			D[v[i]][u[i]] = min(D[v[i]][u[i]], int(b[i]));
+			D[u[i]][v[i]] = min(D[u[i]][v[i]], int(b[i]));
 		}
 		for (int k = 0; k <= n; k++){
 			for (int i = 0; i <= n; i++){
 				for (int j = 0; j <= n; j++){
 					if (D[i][j] > D[i][k] + D[k][j]){
 						D[i][j] = D[i][k] + D[k][j];
-						par[i][j] = par[k][j];
+						path[i][j] = k;
 					}
 				}
 			}
 		}
-		pair<double, double> p ;
+		V<int>ans;
+		pair<int, int> p ;
 		p.first = T[src][sink];
 		p.second = D[src][sink];
-		V<int>ans;
-		while (src != sink){
-			ans.push_back(sink);
-			sink = par[src][sink];
-		}
-		ans.push_back(sink);
-		reverse(all(ans));
-		for (int i = 0; i < ans.size(); i++){
-			printf("%d", ans[i]);
-			if (i + 1 < ans.size()) printf(" ");
-		}
-		puts("");
-		printf("%.1f %.1f\n", p.second, p.first);
+		Path(src, sink,0);
+		cout << fixed << setprecision(1);
+		cout << endl;
+		cout << p.second/10.0 << " " << p.first/10.0 << endl;
 	}
 	return 0;
 }

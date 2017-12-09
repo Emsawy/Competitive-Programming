56da56b870666cbc7c114c4b2bbea82af662a73c
Emsawy <M.EmadAhmed101@gmail.com>
2017-12-09 09:53:34 +0200

    Update CF557-D2-C.cpp

--- Codeforces/CF557-D2-C.cpp
+++ Codeforces/CF557-D2-C.cpp
@@ -119,3 +119,97 @@ int main()
 	}
 	return 0;
 }
+/*
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+void Emsawy(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios::sync_with_stdio(false);   // don't use scanf & printf
+	cin.tie(0);
+	cout.tie(0);
+}
+
+#define p00          point(0,0)
+#define O            const point & 
+#define CD           const double &  
+#define sz(v)	     ((int)((v).size()))
+#define all(v)	     ((v).begin()), ((v).end())
+#define allr(v)	     ((v).rbegin()), ((v).rend())
+#define clr(v,d)     memset(v, d, sizeof(v))
+#define length(a)    (hypot((a).imag(), (a).real()))
+#define pii	         pair<int,int>
+#define V	         vector
+#define MP	         make_pair
+#define bug(n)           cout<< #n <<" = "<< n << endl;
+
+typedef long long ll;
+
+const double PI = acos(-1.0);
+const double eps = 1e-10;
+const ll mod = 1e9 + 7, oo = 1e9;
+
+int dx[]{1, -1, 0, 0, 1, -1, 1, -1};
+int dy[]{0, 0, 1, -1, 1, -1, -1, 1};
+
+int n, m, k, c;
+const int N = 100000 + 5;
+pair<int, int> g[N];
+int cnt[N], p[300], lstp[300];
+
+int main()
+{
+	Emsawy();
+	while (cin >> n){
+		for (int i = 0; i < n; i++){
+			cin >> g[i].first;
+		}
+		for (int i = 0; i < n; i++){
+			cin >> g[i].second;
+		}
+		sort(g, g + n);
+		for (int i = n - 1; i >= 0; i--){
+			cnt[i] = g[i].second;
+			if (i + 1 < n){
+				cnt[i] += cnt[i + 1];
+			}
+		}
+		int ans = oo, maxlen = 0;
+		for (int i = 0; i < n; i++){
+			p[g[i].second] ++;
+			maxlen++;
+			if (g[i].first != g[i + 1].first){
+				int cur = 0, rem = (i + 1) - (maxlen * 2 - 1);
+				if (rem > 0){
+					for (int j = 0; j < 300; j++){
+						if (rem - lstp[j] >= 0){
+							rem -= lstp[j];
+							cur += lstp[j] * j;
+						}
+						else{
+							cur += rem * j;
+							rem = 0;
+							break;
+						}
+					}
+				}
+				ans = min(ans, cnt[i + 1] + cur);
+				for (int j = 0; j < 300; j++){
+					lstp[j] += p[j];
+				}
+				clr(p, 0);
+				maxlen = 0;
+			}
+		}
+		cout << ans << endl;
+	}
+	return 0;
+}
+*/

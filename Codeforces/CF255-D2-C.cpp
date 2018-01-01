86d44cc19088af21796e2edf51ead405f2e019b6
Emsawy <M.EmadAhmed101@gmail.com>
2018-01-01 15:56:42 +0200

    Update CF255-D2-C.cpp

--- Codeforces/CF255-D2-C.cpp
+++ Codeforces/CF255-D2-C.cpp
@@ -61,3 +61,81 @@ int main() {
 	}
 	return 0;
 }
+
+/*
+// Brute force solution
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+ll mod = 1000000007, oo = 1000000009;
+
+void Emsawy() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+#define clr(v,d)     memset(v, d, sizeof(v))
+#define sz(v)	     ((int)((v).size()))
+#define all(v)	     ((v).begin()), ((v).end())
+#define allr(v)	     ((v).rbegin()), ((v).rend())
+#define V	         vector
+#define MP	         make_pair
+#define bug(n)           cout<< #n <<" = "<< n << endl;
+int dx[]{1, -1, 0, 0, 1, -1, 1, -1};
+int dy[]{0, 0, 1, -1, 1, -1, -1, 1};
+
+V<V<pair<int, int> > > adj;
+const ll N = 1000000 + 5;
+ll n, m, k, w;
+map<int, V<int> > mp;
+int valid(V<int>&a, V<int>&b){
+	int ret = 0;
+	int sa = 0, sb = 0, idx = -1;
+	for (sa = 0; sa < a.size(); sa++){
+		if (idx < a[sa]){
+			idx = a[sa];
+			ret++;
+			bool ok = 0;
+			for (; sb < b.size(); sb++){
+				if (idx < b[sb]){
+					ok = 1;
+					idx = b[sb];
+					ret++;
+					break;
+				}
+			}
+			if (ok == 0) return ret;
+		}
+	}
+	return ret;
+}
+int main() {
+
+	Emsawy();
+	while (cin >> n){
+		V<int>v(n);
+		for (int i = 0; i < n; i++){
+			cin >> v[i];
+			mp[v[i]].push_back(i);
+		}
+		int ans = 0;
+		for (map<int, V<int> > ::iterator a = mp.begin(); a != mp.end(); a++){
+			for (map<int, V<int> > ::iterator b = a; b != mp.end(); b++){
+				if (a->first == b->first) ans = max(ans, (int)a->second.size());
+				else ans = max(ans, max(valid(b->second, a->second), valid(a->second, b->second)));
+			}
+		}
+		cout << ans << endl;
+	}
+	return 0;
+}
+
+*/

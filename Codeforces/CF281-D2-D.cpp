a262520ebf966a6d44ecf25c27d3658a1bcc2a6d
Emsawy <M.EmadAhmed101@gmail.com>
2018-01-17 06:48:22 +0200

    Create CF281-D2-D.cpp

---
+++ Codeforces/CF281-D2-D.cpp
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+ll mod = 1000000007, oo = 1000000009;
+
+void Emsawy() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+#define clr(v,d)     memset(v, d, sizeof(v))
+#define sz(v)	     ((int)((v).size()))
+#define all(v)	     ((v).begin()), ((v).end())
+#define allr(v)	     ((v).rbegin()), ((v).rend())
+#define V	         vector
+#define MP	         make_pair
+#define bug(n)           cout<< #n <<" = "<< n << endl;
+int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+int dy[] = { 0, 0, 1, -1, 1, -1, -1, 1 };
+
+V<V<int> > adj;
+int n, m, k, w;
+
+int main() {
+
+	Emsawy();
+	while (cin >> n){
+		stack<int>st;
+		int mx = 0;
+		for (int i = 0; i < n; i++){
+			cin >> m;
+			if (st.empty()){
+				st.push(m);
+				continue;
+			}
+			if (st.top() > m) mx = max(mx, m ^ st.top());
+			while (st.size() && m >= st.top()){
+				mx = max(mx, m ^ st.top());
+				st.pop();
+			}
+			if (st.size())  mx = max(mx, m ^ st.top());
+			st.push(m);
+		}
+		cout << mx << endl;
+	}
+	return 0;
+}

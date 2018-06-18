ff2c3981eb4fc58f06d6a8dce6c0ee224d5e6d4f
Emsawy <M.EmadAhmed101@gmail.com>
2018-06-18 06:56:07 +0200

    Update SRM653-D2-500.cpp

--- Topcoder/SRM653-D2-500.cpp
+++ Topcoder/SRM653-D2-500.cpp
@@ -50,3 +50,60 @@ public:
 		return (Cnr(card.size(), score) * pw(2, card.size() - score)) % mod;
 	}
 };
+
+/*
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+class RockPaperScissorsMagicEasy {
+public:
+	vector<int> _card;
+	int _score;
+
+	int mod = 1000000007;
+
+	int dp[2001][2001];
+	int go(int nd, int score) {
+		if (nd >= _card.size()) {
+			return _score == score;
+		}
+		int &ret = dp[nd][score];
+		if (ret != -1) return ret;
+		ret = 0;
+		ret = (ret + go(nd + 1, score + 1)) % mod;
+		ret = (ret + go(nd + 1, score)) % mod;
+		ret = (ret + go(nd + 1, score)) % mod;
+		return ret;
+	}
+
+	int count(vector <int> card, int score) {
+		_card = card;
+		_score = score;
+		for (int i = 0; i < 2001; i++)
+			for (int j = 0; j < 2001; j++)
+				dp[i][j] = -1;
+
+		return go(0, 0);
+	}
+};
+*/

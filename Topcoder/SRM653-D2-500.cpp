f36072a054132370521b76a1bce4d7f77f968357
Emsawy <M.EmadAhmed101@gmail.com>
2018-06-18 06:54:33 +0200

    Create SRM653-D2-500.cpp

---
+++ Topcoder/SRM653-D2-500.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+typedef long long ll;
+
+class RockPaperScissorsMagicEasy {
+public:
+
+	ll mod = 1000000007;
+	ll fact[2001];
+
+	ll pw(ll b, ll p) {
+		if (p == 0) return 1;
+		if (p == 1) return b;
+		ll ret = pw(b, p / 2);
+		ret = (ret * ret) % mod;
+		if (p % 2)
+			ret = (ret * b) % mod;
+		return ret;
+	}
+
+	ll Cnr(int n, int m) {
+		return (fact[n] * pw( (fact[n - m] * fact[m]) % mod , mod - 2)) % mod;
+	}
+
+	int count(vector <int> card, int score) {
+		if (card.size() < score) return 0;
+		fact[0] = fact[1] = 1;
+		for (int i = 2; i < 2001; i++)
+			fact[i] = (fact[i - 1] * 1LL * i) % mod;
+
+		return (Cnr(card.size(), score) * pw(2, card.size() - score)) % mod;
+	}
+};
